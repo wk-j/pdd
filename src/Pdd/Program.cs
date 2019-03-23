80ac38698ab9381e0b48901c4a6e0e478539f420
wk-j <somnuk.wk@gmail.com>
2019-03-23 22:30:58 +0700

    Added todo

--- src/Pdd/Program.cs
+++ src/Pdd/Program.cs
@@ -4,6 +4,8 @@ namespace Pdd
 {
     class Program
     {
+        // @todo Remove hello world!
+        //  Remove console write line from source code
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
