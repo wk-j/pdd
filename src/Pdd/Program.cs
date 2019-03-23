23923db75b7c89c81cd1309dc98ec5a78b14ddd2
wk-j <somnuk.wk@gmail.com>
2019-03-23 22:22:47 +0700

    Initial commit

---
+++ src/Pdd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
