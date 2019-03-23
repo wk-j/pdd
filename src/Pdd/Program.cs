1653529ea9fa57a59f1d9973387b28cabd74631c
wk-j <somnuk.wk@gmail.com>
2019-03-23 23:14:27 +0700

    Refactor

--- src/Pdd/Program.cs
+++ src/Pdd/Program.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Linq;
 
 namespace Pdd {
     class Program {
         // @todo Convert to asyc
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            if (args.Length > 0) {
+                Console.WriteLine("Hello World! - {0}", args.First());
+            }
         }
     }
 }
